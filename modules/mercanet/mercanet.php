51dc07a412450ba2df04aaa2cd86a7f3ba30036e
Eddy <rakoto.tje@gmail.com>
2023-05-01 14:06:52 +0300

    fix compatibility mercarnet

--- modules/mercanet/mercanet.php
+++ modules/mercanet/mercanet.php
@@ -1603,6 +1603,7 @@ class Mercanet extends PaymentModule
      */
     public function hookPayment($params)
     {
+        die('debug payment');
         // Check if a payment can be display
         if (!$this->canDisplayPayment()) {
             return false;

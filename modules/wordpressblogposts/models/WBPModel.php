4ca4763c29d1533e32328d2afe3684cf69bbfadb
Eddy <rakoto.tje@gmail.com>
2023-06-05 08:31:34 +0300

    flag blog

--- modules/wordpressblogposts/models/WBPModel.php
+++ modules/wordpressblogposts/models/WBPModel.php
@@ -160,6 +160,7 @@ class WBPModel {
         $recent_posts = wp_get_recent_posts( $args);
         $posts = array();
         foreach($recent_posts as $post){
+            dump($post);die;
             $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post['ID'] ));
             $posts[] = array(
                 'url' => get_permalink($post['ID']),

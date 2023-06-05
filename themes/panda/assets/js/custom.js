04b5c8a024a4d3d8b4511d0032bb5e050726e8cf
Eddy <rakoto.tje@gmail.com>
2023-06-05 22:59:40 +0300

    update

--- themes/panda/assets/js/custom.js
+++ themes/panda/assets/js/custom.js
@@ -8,5 +8,4 @@ jQuery(function ($) {
     $('#st_ma_381').addClass('is_parent');
     $('.stfeaturedslider_container').removeClass('block');
     $('.easycontent_container').removeClass('block');
-    $('.swiper_container_1').addClass('col-lg-12');
 })

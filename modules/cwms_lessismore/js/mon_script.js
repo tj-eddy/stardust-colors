64b456efae0703eae6d9d7fb3d9802ef01f3dd67
Eddy <rakoto.tje@gmail.com>
2023-06-12 14:37:33 +0300

    show more

--- modules/cwms_lessismore/js/mon_script.js
+++ modules/cwms_lessismore/js/mon_script.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 
 var conf_less = JSON.parse(lessismore_conf.replace(/&quot;/g,'"'));
 for (let i = 0; i < conf_less.length; i++) {
-    $(conf_less[i].selector).readmore({ speed: conf_less[i].speed, collapsedHeight: parseInt(conf_less[i].collapsedHeight), moreLink: '<span class="cursor_show_more" style="cursor:pointer;">'+conf_less[i].moreLink+'</span>', lessLink: '<span style="cursor:pointer;">'+conf_less[i].lessLink+'</span>' });
+    $(conf_less[i].selector).readmore({ speed: conf_less[i].speed, collapsedHeight: parseInt(conf_less[i].collapsedHeight), moreLink: '<span class="cursor_show_more" style="cursor:pointer;">'+conf_less[i].moreLink+'</span>', lessLink: '<span class="cursor_show_more" style="cursor:pointer;">'+conf_less[i].lessLink+'</span>' });
 }
 
 
